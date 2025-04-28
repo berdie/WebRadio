bd5f1d730bc19c5f1b5be3604440b33571b31593
bertuccodiego <44110003+berdie@users.noreply.github.com>
2025-04-28 13:47:59 +0200

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace RadioStreamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialComboBox1 = new MaterialSkin.Controls.MaterialComboBox();
+            this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            this.materialButton2 = new MaterialSkin.Controls.MaterialButton();
+            this.materialSlider2 = new MaterialSkin.Controls.MaterialSlider();
+            this.SuspendLayout();
+            // 
+            // materialComboBox1
+            // 
+            this.materialComboBox1.AutoResize = false;
+            this.materialComboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialComboBox1.Depth = 0;
+            this.materialComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.materialComboBox1.DropDownHeight = 174;
+            this.materialComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.materialComboBox1.DropDownWidth = 121;
+            this.materialComboBox1.Font = new System.Drawing.Font("Roboto Medium", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.materialComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialComboBox1.FormattingEnabled = true;
+            this.materialComboBox1.IntegralHeight = false;
+            this.materialComboBox1.ItemHeight = 43;
+            this.materialComboBox1.Location = new System.Drawing.Point(6, 77);
+            this.materialComboBox1.MaxDropDownItems = 4;
+            this.materialComboBox1.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialComboBox1.Name = "materialComboBox1";
+            this.materialComboBox1.Size = new System.Drawing.Size(281, 49);
+            this.materialComboBox1.StartIndex = 0;
+            this.materialComboBox1.TabIndex = 0;
+            this.materialComboBox1.SelectedIndexChanged += new System.EventHandler(this.materialComboBox1_SelectedIndexChanged);
+            // 
+            // materialButton1
+            // 
+            this.materialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton1.Depth = 0;
+            this.materialButton1.HighEmphasis = true;
+            this.materialButton1.Icon = null;
+            this.materialButton1.Location = new System.Drawing.Point(294, 89);
+            this.materialButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton1.Name = "materialButton1";
+            this.materialButton1.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton1.Size = new System.Drawing.Size(64, 36);
+            this.materialButton1.TabIndex = 1;
+            this.materialButton1.Text = "PLAY";
+            this.materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton1.UseAccentColor = false;
+            this.materialButton1.UseVisualStyleBackColor = true;
+            // 
+            // materialButton2
+            // 
+            this.materialButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton2.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton2.Depth = 0;
+            this.materialButton2.HighEmphasis = true;
+            this.materialButton2.Icon = null;
+            this.materialButton2.Location = new System.Drawing.Point(366, 89);
+            this.materialButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton2.Name = "materialButton2";
+            this.materialButton2.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton2.Size = new System.Drawing.Size(64, 36);
+            this.materialButton2.TabIndex = 2;
+            this.materialButton2.Text = "STOP";
+            this.materialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton2.UseAccentColor = false;
+            this.materialButton2.UseVisualStyleBackColor = true;
+            // 
+            // materialSlider2
+            // 
+            this.materialSlider2.Depth = 0;
+            this.materialSlider2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialSlider2.Location = new System.Drawing.Point(6, 134);
+            this.materialSlider2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSlider2.Name = "materialSlider2";
+            this.materialSlider2.Size = new System.Drawing.Size(424, 40);
+            this.materialSlider2.TabIndex = 4;
+            this.materialSlider2.Text = "Volume";
+            this.materialSlider2.Click += new System.EventHandler(this.materialSlider2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 191);
+            this.Controls.Add(this.materialSlider2);
+            this.Controls.Add(this.materialButton2);
+            this.Controls.Add(this.materialButton1);
+            this.Controls.Add(this.materialComboBox1);
+            this.Name = "Form1";
+            this.Text = "Web Radio Station";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialComboBox materialComboBox1;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
+        private MaterialSkin.Controls.MaterialButton materialButton2;
+        private MaterialSkin.Controls.MaterialSlider materialSlider2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using NAudio.Wave;
+
+namespace RadioStreamer
+{
+    public partial class Form1 : MaterialForm
+    {
+        private readonly MaterialSkin.MaterialSkinManager materialSkinManager;
+        private List<RadioStation> radioStations = new List<RadioStation>();
+        private IWavePlayer waveOut;
+        private MediaFoundationReader mediaReader;
+        private bool isPlaying = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            materialButton1.Click += (s, e) => PlaySelectedStation();
+            materialButton2.Click += (s, e) => StopPlayback();
+            materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(
+                MaterialSkin.Primary.Indigo700, MaterialSkin.Primary.Indigo500,
+                MaterialSkin.Primary.Indigo100, MaterialSkin.Accent.Pink700,
+                MaterialSkin.TextShade.WHITE
+            );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Carica le stazioni radio dal file CSV
+            LoadRadioStations();
+
+            // Popola il ComboBox con le stazioni
+            materialComboBox1.DataSource = radioStations;
+
+            // Inizializza il player audio
+            waveOut = new WaveOut();
+
+            // Imposta il volume iniziale
+            if (materialSlider2 != null)
+            {
+                waveOut.Volume = materialSlider2.Value / 100f;
+            }
+        }
+
+        private void LoadRadioStations()
+        {
+            try
+            {
+                string filePath = Path.Combine(Application.StartupPath, "InternetRadio.csv");
+                if (File.Exists(filePath))
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+
+                    // Salta l'intestazione
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+                        string line = lines[i];
+                        string[] parts = line.Split(',');
+                        if (parts.Length >= 3)
+                        {
+                            int id = int.Parse(parts[0]);
+                            radioStations.Add(new RadioStation(id, parts[1].Trim(), parts[2].Trim()));
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("File InternetRadio.csv non trovato!", "Errore",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Errore durante il caricamento delle stazioni radio: {ex.Message}",
+                    "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void materialComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Arresta la riproduzione corrente quando l'utente cambia stazione
+            StopPlayback();
+        }
+
+        private void materialSlider2_Click(object sender, EventArgs e)
+        {
+            // Usa il materialSlider2 come controllo del volume
+            if (waveOut != null)
+            {
+                waveOut.Volume = materialSlider2.Value / 100f;
+            }
+        }
+
+        // Aggiungiamo un metodo per avviare la riproduzione
+        public void PlaySelectedStation()
+        {
+            if (materialComboBox1.SelectedItem == null) return;
+
+            try
+            {
+                StopPlayback();
+
+                RadioStation selectedStation = (RadioStation)materialComboBox1.SelectedItem;
+                mediaReader = new MediaFoundationReader(selectedStation.Url);
+                waveOut.Init(mediaReader);
+                waveOut.Play();
+                isPlaying = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Errore durante la riproduzione: {ex.Message}",
+                    "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Metodo per arrestare la riproduzione
+        public void StopPlayback()
+        {
+            if (isPlaying)
+            {
+                waveOut.Stop();
+                mediaReader.Dispose();
+                mediaReader = null;
+                isPlaying = false;
+            }
+        }
+
+        // Assicuriamoci di liberare le risorse quando il form viene chiuso
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            StopPlayback();
+            if (waveOut != null)
+            {
+                waveOut.Dispose();
+                waveOut = null;
+            }
+            base.OnFormClosing(e);
+        }
+    }
+
+    // Aggiornata la classe RadioStation per includere l'ID
+    public class RadioStation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public RadioStation(int id, string name, string url)
+        {
+            Id = id;
+            Name = name;
+            Url = url;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
